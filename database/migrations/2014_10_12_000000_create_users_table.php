77c36675c9a3fe061e7a7a81f73e2bc583cdc4c8
Jonidohc16v <118388906+Jonidohc16v@users.noreply.github.com>
2023-05-15 16:23:29 +0200

    user database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        //create the user table on the DB
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
