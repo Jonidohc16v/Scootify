97c29d899738e8c22cdc4afe1a0c6029b286beed
Jonidohc16v <118388906+Jonidohc16v@users.noreply.github.com>
2023-05-15 21:41:27 +0200

    users table update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('phone_number')->unique();
+            $table->string('address');
+            $table->integer('isAdmin');
             $table->rememberToken();
             $table->timestamps();
         });
