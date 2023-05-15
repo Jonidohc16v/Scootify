710ee4e86fad59a0f5aee30a6c52e329a575061f
ardnasngn <nguyensandra6@gmail.com>
2023-05-15 20:36:33 +0200

    change display and text of navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,15 +7,11 @@
     <title>Scootify - Your Daily Ride !</title>
 
     <!-- Font Awesome -->
-<link
-href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-rel="stylesheet"
-/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <!-- Google Fonts -->
-<link
-href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-rel="stylesheet"
-/>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Play&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 <!-- MDB -->
 <link
@@ -31,9 +27,10 @@ rel="stylesheet"
 
 <style>
     nav{
-    
-
+        font-family: 'Play', sans-serif;
+        text-align: center;
     }
+
     /*Code to change color of active link*/
     .navbar-nav .active a {
         color: #34b38a;
@@ -43,22 +40,22 @@ rel="stylesheet"
 </head>
 
 <body>
-    
         <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <!-- Container wrapper -->
-    <div class="container">
+    <div class="container-fluid">
       <!-- Navbar brand -->
-        <a class="navbar-brand me-10" href="/">
-            <img
-              src="{{asset('images/logo.png')}}"
-              height="95"
-              alt="Logo"
-              loading="logo"
-              style="margin-top: -1px; margin-left: -100px"
-            />
-          </a>
-  
+      <a class="navbar-brand me-5" href="/">
+        <img
+          src="{{asset('images/logo.png')}}"
+          height="90rem"
+          alt="Logo"
+          loading="logo"
+          id="logo"
+          style="margin-top:-0.8rem; margin-bottom: -0.6rem"
+        />
+      </a>
+
       <!-- Toggle button -->
       <button
         class="navbar-toggler"
@@ -71,38 +68,48 @@ rel="stylesheet"
       >
         <i class="fas fa-bars"></i>
       </button>
-  
+
       <!-- Collapsible wrapper -->
       <div class="collapse navbar-collapse" id="navbarButtonsExample">
 
-           <!-- Left links -->
-    <ul class="navbar-nav d-flex align-items-right me-auto mb-2 mb-lg-0" 
-    style="margin-top: -1px; margin-left: 250px">
-        <li class="nav-item disable">
-          <a class="nav-link" href="#">How it works?</a>
-        </li>
-        <li class="nav-item disable">
-          <a class="nav-link" href="#">Price</a>
-        </li>
-        <li class="nav-item disable">
-          <a class="nav-link" href="#">Find a Station</a>
-        </li>
-        
-      </ul>
-      <!-- Left links -->
-            
-        </div>
-           <div class="d-flex align-items-center"
-           style="margin-top: -1px; margin-right: -100px">
-          <button type="button" class="btn btn-primary me-4" style="background-color: #34b38a">
-            <i class="fa-solid fa-user"></i> Login
+        <!-- Left links -->
+        <ul class="navbar-nav d-flex align-items-center mx-auto mb-2 mb-lg-0" style="gap: 4rem">
+          <li class="nav-item disable">
+            <a class="nav-link" href="/how-it-works">
+              <span class="material-symbols-outlined">
+                electric_scooter
+              </span> <br>
+              HOW IT WORKS?</a>
+          </li>
+          <li class="nav-item disable">
+            <a class="nav-link" href="/price">
+              <span class="material-symbols-outlined">
+                credit_card
+              </span><br>
+              PRICE</a>
+          </li>
+          <li class="nav-item disable">
+            <a class="nav-link" href="/stations">
+              <span class="material-symbols-outlined">
+                location_on
+              </span><br>
+              FIND A STATION</a>
+          </li>
+        </ul>
+        <!-- Left links -->
+
+        <div class="ms-auto d-flex align-items-center">
+          <button type="button" class="btn btn-primary me-3" style="background-color: #34b38a">
+            <i class="fa-solid fa-user"> </i> Login
           </button>
           <button type="button" class="btn btn-primary me-3" style="background-color: #34b38a">
-            <i class="fa-solid fa-phone"></i> Contact Us
+            <i class="fa-solid fa-phone"> </i> Contact Us
           </button>
+        </div>
+
       </div>
       <!-- Collapsible wrapper -->
-      
+
     </div>
     <!-- Container wrapper -->
   </nav>
