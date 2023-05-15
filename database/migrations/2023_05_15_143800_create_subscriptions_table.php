8f1f7ec9386a89bc2024fe0eec64a657cceb28df
Ninitas2022 <120451320+Ninitas2022@users.noreply.github.com>
2023-05-15 16:41:23 +0200

    DB

--- database/migrations/2023_05_15_143800_create_subscriptions_table.php
+++ database/migrations/2023_05_15_143800_create_subscriptions_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->foreignId('user_id');
+            $table->integer('start_date');
+            $table->integer('end_date');
+            $table->enum('payment_method', ['card', 'cash']);
+            $table->integer('status');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('subscriptions');
     }
 };
---
+++ resources/views/index.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Scootify - Your Daily Ride !</title>
+
+    <!-- Font Awesome -->
+<link
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.0/mdb.min.css"
+rel="stylesheet"
+/>
+
+<!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.0/mdb.min.js"
+></script>
+</head>
+<body class="d-flex flex-column min-vh-100">
+    
+<footer class="mt-auto">
+
+<section class="">
+  <!-- Footer -->
+  <footer class="text-center text-white" style="background-color: #5ccfa9;">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: CTA -->
+
+      <section class="d-flex justify-content-center text-align-center gap-5">
+        <p><img src="{{asset('images/logo-1.png')}}" class="rounded" style="width:40px" alt="logo"></p>
+        <p class=""><a href=""><span class="text-dark text-align-center">FAQ</span></a></p>
+        <p class=""><a href=""><span class="text-dark">Contact us!</span> </a></p> 
+      </section>
+      <!-- Section: CTA -->
+    </div>
+    <!-- Grid container -->
+
+    
+    <div class="text-center d-flex justify-content-around ml-50" style="background-color: rgba(0, 0, 0, 0.2);" >
+    <section class="p-1 text-center p-100" style="margin-right:-1500px">
+      <!-- Facebook -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-facebook-f"></i
+      ></a>
+
+      <!-- Twitter -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-twitter"></i
+      ></a>
+
+      <!-- Instagram -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-instagram"></i
+      ></a>
+      </section> 
+
+      <!-- Back to top button -->
+      <div class="d-flex justify-content-end position-relative" style="left:620px">
+        <button type="button" class="btn btn-danger btn-floating btn-lg d-flex justify-content-end position-relative" id="btn-back-to-top">
+            <i class="fas fa-arrow-up"></i>
+        </button>
+        </div>
+      
+</div>
+</section>
+</footer>
+</body>
+</html>
