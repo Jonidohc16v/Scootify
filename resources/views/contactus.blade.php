982f20fc21bf27349c0cbb9f82e4d8abd181f5b4
ardnasngn <nguyensandra6@gmail.com>
2023-05-16 23:22:57 +0200

    change to responsive

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -1,89 +1,80 @@
-
 <x-layout>
-<!--Section: Contact v.2-->
-<section class="mb-6" style="width:55rem ; margin:5rem auto 5rem; padding: 2rem; box-shadow: 0 0 20px #34b38a">
-
-    <div style="margin-y:20rem; background-color:#68d0b0">
+    <!--Section: Contact v.2-->
+    <section class="mb-6" style="width: 70%; margin: 5rem auto 5rem; padding: 2rem; box-shadow: 0 0 20px #34b38a">
+      <div style="margin-y: 20rem; background-color: #68d0b0">
         <img src="/images/logo-1.png" style="width: 6rem">
-    </div>
-
-    <!--Section heading-->
-    <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
-    <!--Section description-->
-    <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
-        a matter of hours to help you.</p>
-
-    <div class="row">
-
+      </div>
+  
+      <!--Section heading-->
+      <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+      <!--Section description-->
+      <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly.
+        Our team will come back to you within a matter of hours to help you.</p>
+  
+      <div class="row">
         <!--Grid column-->
-        <div class="col-md-9 mb-md-0 mb-5" style="margin-top:2rem">
-            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
-
-                <!--Grid row-->
-                <div class="row">
-
-                    <!--Grid column-->
-                    <div class="col-md-6">
-                        <div class="md-form mb-0">
-                            <input type="text" id="name" name="name" class="form-control">
-                            <label for="name" class="">Your name</label>
-                        </div>
-                    </div>
-                    <!--Grid column-->
-
-                    <!--Grid column-->
-                    <div class="col-md-6">
-                        <div class="md-form mb-0">
-                            <input type="text" id="email" name="email" class="form-control">
-                            <label for="email" class="">Your email</label>
-                        </div>
-                    </div>
-                    <!--Grid column-->
-
+        <div class="col-lg-9 col-md-12 mb-md-0 mb-5">
+          <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+  
+            <!--Grid row-->
+            <div class="row">
+              <!--Grid column-->
+              <div class="col-md-6">
+                <div class="md-form mb-0">
+                  <input type="text" id="name" name="name" class="form-control">
+                  <label for="name" class="">Your name</label>
                 </div>
-                <!--Grid row-->
-
-                <!--Grid row-->
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="md-form mb-0">
-                            <input type="text" id="subject" name="subject" class="form-control">
-                            <label for="subject" class="">Subject</label>
-                        </div>
-                    </div>
+              </div>
+              <!--Grid column-->
+  
+              <!--Grid column-->
+              <div class="col-md-6">
+                <div class="md-form mb-0">
+                  <input type="text" id="email" name="email" class="form-control">
+                  <label for="email" class="">Your email</label>
                 </div>
-                <!--Grid row-->
-
-                <!--Grid row-->
-                <div class="row">
-
-                    <!--Grid column-->
-                    <div class="col-md-12">
-
-                        <div class="md-form">
-                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
-                            <label for="message">Your message</label>
-                        </div>
-
-                    </div>
+              </div>
+              <!--Grid column-->
+            </div>
+            <!--Grid row-->
+  
+            <!--Grid row-->
+            <div class="row">
+              <div class="col-md-12">
+                <div class="md-form mb-0">
+                  <input type="text" id="subject" name="subject" class="form-control">
+                  <label for="subject" class="">Subject</label>
                 </div>
-                <!--Grid row-->
-
-            </form>
-
-            <div class="text-center text-md-left">
-                <a class="btn btn-primary" style="background-color: #34b38a" onclick="document.getElementById('contact-form').submit();">Send</a>
+              </div>
+            </div>
+            <!--Grid row-->
+  
+            <!--Grid row-->
+            <div class="row">
+              <!--Grid column-->
+              <div class="col-md-12">
+                <div class="md-form">
+                  <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                  <label for="message">Your message</label>
+                </div>
+              </div>
+            </div>
+            <!--Grid row-->
+  
+            <div class="text-center text-md-left mt-4">
+              <button class="btn btn-primary" style="background-color: #34b38a" type="submit">Send</button>
             </div>
             <div class="status"></div>
+          </form>
         </div>
         <!--Grid column-->
-
+  
         <!--Grid column-->
-        <div class="col-md-3 text-center">
-            <ul class="list-unstyled mb-0">
-                <li><i class="fas fa-map-marker-alt fa-2x"></i>
-                    <p>7 Porte de France, L-4360 ESCH-SUR-ALZETTE, Luxembourg</p>
-                </li>
+        <div class="col-lg-3 col-md-12 text-center">
+          <ul class="list-unstyled mb-0">
+            <li><i class="fas fa-map-marker-alt fa-2x"></i>
+              <p>7 Porte de France, L-4360 ESCH-SUR-ALZETTE, Luxembourg</p>
+            </li>
 
                 <li><i class="fas fa-phone mt-4 fa-2x"></i>
                     <p>+ 352 234 567 891</p>
