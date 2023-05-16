44c0d9350f78618323216aa04716d24c6b7c7d40
ardnasngn <120388381+ardnasngn@users.noreply.github.com>
2023-05-16 22:49:29 +0200

    Update intro_card.blade.php

--- resources/views/components/intro_card.blade.php
+++ resources/views/components/intro_card.blade.php
@@ -1,4 +1,4 @@
-<div class="card bg-dark text-dark mx-auto mt-4" style="width: 70%; text-shadow: #34b38a 1px 0 10px;">
+<div class="card bg-dark text-white mx-auto mt-4" style="width: 70%; text-shadow: #34b38a 1px 0 10px;">
   <img src="/images/homepage-image.jpg" class="card-img img-responsive" alt="Scootify ride"/>
   <div class="card-img-overlay" style="text-align:left; padding:5rem 1rem">
     <h1 class="card-title" style="color: #0e1013">SCOOTIFY</h1>
