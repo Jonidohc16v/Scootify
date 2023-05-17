bafd9206879374eb5ba6e415bfc107da88460e7b
Ninitas2022 <120451320+Ninitas2022@users.noreply.github.com>
2023-05-17 09:23:55 +0200

    new card

--- resources/views/components/homepage.blade.php
+++ resources/views/components/homepage.blade.php
@@ -1,40 +1,33 @@
-<div class="card mt-5 mb-7 align-items-center shadow-none">
-
-<div class="mt-3 mb-5">
-  <h2>Scootify Statistics</h2>
-</div>
-
-  <div class="row gap-5 p-1 w-50">
-    <div class="card col-sm rounded-circle"  style="box-shadow: 0 0 20px #34b38a;">
-      <div class="card-body text-center align-items-center d-flex flex-column justify-content-center">
-        <span class="material-symbols-outlined" style="font-size:3rem;">
+<div class="container mt-5 mb-7 d-flex justify-content-center">
+  <div class="row gap-5">
+<div class="card col-12 col-sm-4 rounded-circle" style="width: 12rem; box-shadow: 0 0 20px #34b38a">
+  <div class="card-body">
+  <span class="material-symbols-outlined" style="font-size:3rem;">
           ev_station
         </span>
-        <br>
-        <h5>Stations</h5>
-        <p>5</p>
-      </div>
-    </div>
-    <div class="card col-sm rounded-circle" style="box-shadow: 0 0 20px #34b38a;">
-      <div class="card-body text-center align-items-center d-flex flex-column justify-content-center">
-      <span class="material-symbols-outlined" style="font-size:3rem;">
-electric_scooter
-</span> 
-        <br>
-        <h5>Scooters</h5>
-        <p>50</p>
-      </div>
-    </div>
-    <div class="card col-sm rounded-circle" style="box-shadow: 0 0 20px #34b38a;">
-      <div class="card-body text-center align-items-center d-flex flex-column justify-content-center">
-        
-          <span class="material-symbols-outlined" style="font-size:3rem;">
-            sports_martial_arts
-          </span>
-        <br>
-        <h5>Users</h5>
-        <p>363</p>
-      </div>
-    </div>
+    <h5 class="card-title">Stations</h5>
+    <p class="card-text">5</p>
   </div>
 </div>
+<div class="card col-12 col-sm-4 rounded-circle" style="width: 12rem; box-shadow: 0 0 20px #34b38a">
+  <div class="card-body">
+  <span class="material-symbols-outlined" style="font-size:3rem;">
+electric_scooter
+</span> 
+    <h5 class="card-title">Scooters</h5>
+    <p class="card-text">50</p>
+  </div>
+</div>
+<div class="card col-12 col-sm-4 rounded-circle" style="width: 12rem; box-shadow: 0 0 20px #34b38a">
+  <div class="card-body">
+  <span class="material-symbols-outlined" style="font-size:3rem;">
+            sports_martial_arts
+          </span>
+    <h5 class="card-title">Users</h5>
+    <p class="card-text">363</p>
+  </div>
+</div>
+</div>
+</div>
+
+
