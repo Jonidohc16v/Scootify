5d31cc624d7cc4bd70f131950e7a1defaaf4ef50
Ninitas2022 <120451320+Ninitas2022@users.noreply.github.com>
2023-05-19 16:28:46 +0200

    edit user

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -1,15 +1,21 @@
 <x-layout>
 
+<style>
 
+
+</style>
                     <!-- User Info -->
 
-<div class="card mt-5 m-auto" style="width:65%; box-shadow: 0 0 20px #34b38a">
+<div class="card mt-5 m-auto" style="width:65%; box-shadow: 0 0 20px #34b38a;">
   <div class="card-body">
-    <h5 class="card-title" style="font-size:2rem;">Username</h5>
-    <p class="card-text">reinaldo8@gmail.com</p>
-    <p class="card-text">+352691555666</p>
-    <p class="card-text">7, Rue de Strasbourg- 1020 Luxembourg</p>
-    <button type="button" class="btn btn-primary" style="background-color: #34b38a">Edit</button>
+  @auth
+    <p class="card-title" style="font-size:2rem;"><h3>{{auth()->user()->name}}</h3></p>
+    <p class="card-text">{{auth()->user()->email}}</p>
+    <p class="card-text">{{auth()->user()->address}}</p>
+    <p class="card-text">{{auth()->user()->phone_number}}</p>
+    <a href="/edit" class="btn btn-primary" style="background-color: #34b38a">Edit</a>
+    @endauth
+  
   </div>
 </div>
 
@@ -24,7 +30,7 @@
       <div class="card" style="height: 400px; box-shadow: 0 0 20px #34b38a">
         <div class="card-body">
           <h5 class="card-title" style="font-size: 2rem;">My Journeys</h5>
-          <ul class="card-text">
+          <ul class="card-text" style="list-style-type:none;">
             <li></li>
           </ul>
         </div>
@@ -37,7 +43,7 @@
       <div class="card" style="height: 400px; box-shadow: 0 0 20px #34b38a">
         <div class="card-body">
           <h5 class="card-title" style="font-size: 2rem;">My Payments</h5>
-          <ul class="card-text">
+          <ul class="card-text" style="list-style-type:none;">
             <li></li>
           </ul>
         </div>
