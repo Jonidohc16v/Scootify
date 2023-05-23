8d4cf0d08dab6b3fd8efdcaa9a4ce352cd18f394
Ninitas2022 <120451320+Ninitas2022@users.noreply.github.com>
2023-05-23 15:42:13 +0200

    MESSAGE UPDATE

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -119,6 +119,6 @@ public function success(Request $request)
 
     $user = User::findOrFail($subscription->user_id);
 
-    return redirect('/user')->with('success', 'Payment successful' .$user->name);
+    return redirect('/user')->with('success', 'Payment successful' . " " . $user->name);
 }
 };
