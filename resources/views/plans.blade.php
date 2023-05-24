8e3e4125f34119e67a03530b97ad28dc897f643c
Ninitas2022 <120451320+Ninitas2022@users.noreply.github.com>
2023-05-24 10:58:34 +0200

    plans update

--- resources/views/plans.blade.php
+++ resources/views/plans.blade.php
@@ -1,327 +1,128 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Scootify - Your Daily Ride !</title>
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Play&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <!-- MDB -->
-    <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.0/mdb.min.css"
-    rel="stylesheet"
-    />
-
-    <!-- MDB -->
-    <script
-      type="text/javascript"
-      src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.0/mdb.min.js">
-    </script>
-
-    <style>
-    body{
-        font-family: 'Play', sans-serif;
-        text-align: center;
-    }
-    
-    /*Code to change color of active link*/
-    .navbar-nav li a {
-    color: black;
-    }
-    .navbar-nav li.active a {
-    color: #34b38a;
-    }
-    
-    .selected {
-        transform: scale(1.05);
-        border: 2px solid #34b38a;
-    }
+<x-layout>
+  <style>
+  body{
+      font-family: 'Play', sans-serif;
+      text-align: center;
+  }
+  
+  /*Code to change color of active link*/
+  .navbar-nav li a {
+  color: black;
+  }
+  .navbar-nav li.active a {
+  color: #34b38a;
+  }
+  
+  .selected {
+      transform: scale(1.05);
+      border: 2px solid #34b38a;
+  }
 </style>
-</head>
+
 
 <body class="d-flex flex-column min-vh-100">
 
 
-    <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-<!-- Container wrapper -->
-<div class="container-fluid">
-  <!-- Navbar brand -->
-  <a class="navbar-brand me-5" href="/index">
-    <img
-      src="{{asset('images/logo.png')}}"
-      height="90rem"
-      alt="Logo"
-      loading="logo"
-      id="logo"
-      style="margin-top:-0.8rem; margin-bottom: -0.6rem"
-    />
-  </a>
-
-  <!-- Toggle button -->
-  <button
-    class="navbar-toggler"
-    type="button"
-    data-mdb-toggle="collapse"
-    data-mdb-target="#navbarButtonsExample"
-    aria-controls="navbarButtonsExample"
-    aria-expanded="false"
-    aria-label="Toggle navigation"
-  >
-    <i class="fas fa-bars"></i>
-  </button>
-
-  <!-- Collapsible wrapper -->
-  <div class="collapse navbar-collapse" id="navbarButtonsExample">
-
-    <!-- Left links -->
-    <ul class="navbar-nav d-flex align-items-center mx-auto mb-2 mb-lg-0" style="gap: 4rem;">
-      <li class="nav-item">
-        <a class="nav-link" href="/how-it-works">
-          <span class="material-symbols-outlined">
-            electric_scooter
-          </span> <br>
-          HOW IT WORKS?</a>
-      </li>
-      @auth
-          <li class="nav-item">
-            <a class="nav-link" href="/plans">
-              <span class="material-symbols-outlined">
-                credit_card
-              </span><br>
-              PLANS</a>
-          </li>
-          @else
-          <li class="nav-item">
-            <a class="nav-link" href="/price">
-              <span class="material-symbols-outlined">
-                credit_card
-              </span><br>
-              PRICE</a>
-          </li>
-          @endauth
-      <li class="nav-item">
-        <a class="nav-link" href="/stations">
-          <span class="material-symbols-outlined">
-            location_on
-          </span><br>
-          FIND A STATION</a>
-      </li>
-    </ul>
-    <!-- Left links -->
-
-    <!-- Logout button -->
-    <div class="ms-auto d-flex align-items-center">
-      @auth
-        <a href="/user">
-          <span class="font-bold uppercase" style="color: black; margin-right:35px;">
-          Welcome {{auth()->user()->name}}
-          </span>
-        </a>
-        <form class="inline" method="POST" action="/logout">
-          @csrf
-          <button type="submit"
-                class="btn btn-primary me-3" 
-                style="background-color: #9b2b1e">>
-                <i class="fa-solid fa-door-closed"></i>
-                Log Out
-          </button>
-        </form>          
-      @else
-        <a href="/login">
-          <button type="button" 
-                class="btn btn-primary me-3" 
-                style="background-color: #34b38a">
-                <i class="fa-solid fa-user"> </i> Login
-          </button>
-        </a>
-      @endauth
-
-      <a href="/contactus">
-        <button type="button" 
-             class="btn btn-primary me-3" 
-             style="background-color: #34b38a">
-        <i class="fa-solid fa-phone"> </i> Contact Us
-         </button>
-      </a>
-    </div>
-
-  </div>
-  <!-- Collapsible wrapper -->
-
-</div>
-<!-- Container wrapper -->
-</nav>
-<!-- Navbar -->
+ 
 
 
 <main>
-  @if (session('error'))
-    <div class="alert alert-danger">
-        {{ session('error') }}
-    </div>
+@if (session('error'))
+  <div class="alert alert-danger">
+      {{ session('error') }}
+  </div>
 @endif
 
 
 
-    
-        <div class="row row-cols-1 row-cols-md-3 g-4 container-fluid" style="height:30rem; width: 60%; margin: 1rem auto">
-            @foreach($plans as $plan)
-            <div class="col">
-                <div class="card h-100" style="box-shadow: 0 0 20px #34b38a" onclick="selectCard({{ $plan->id }})">
-                    <div class="card-body" style="margin-top: 45%; text-align:center">
-                        <h2 class="card-title pb-3"><b>{{ $plan->name }}</b></h2>
-                        <h5>{{ $plan->description }}</h5>
-                        <p style="font-size:4rem">{{ $plan->price }}€</p>
-                        <hr style="font-size: 1rem; border:1px solid #34b38a;">
-                        
-                        <form action="{{ route('checkout', ['plan_id' => $plan->id]) }}" method="POST">
-                          @csrf
-                          <input type="hidden" name="plan_id" value="{{ $plan->id }}">
-                          <button type="submit">Checkout</button>
-                      </form>
-                    </div>
-                </div>
-                
-            </div>
-            @endforeach
-        </div>
-        <p id="error-message" style="color: red; display: none;">Please select a subscription option.</p>
-        <br>
-    
+  
+      <div class="row row-cols-1 row-cols-md-3 g-4 container-fluid" style="height:30rem; width: 60%; margin:5rem auto">
+          @foreach($plans as $plan)
+          <div class="col">
+              <div class="card h-100" style="box-shadow: 0 0 20px #34b38a" onclick="selectCard({{ $plan->id }})">
+                  <div class="card-body" style="margin-top: 5%; text-align:center">
+                    <span class="material-symbols-outlined mb-3" style="font-size:4rem;">
+                      calendar_month
+                      </span>
+                      <h2 class="card-title pb-3"><b>{{ $plan->name }}</b></h2>
+                      <h5>{{ $plan->description }}</h5>
+                      <p style="font-size:4rem">{{ $plan->price }}€</p>
+                      <hr style="font-size: 1rem; border:1px solid #34b38a;">
+                      
+                      <form action="{{ route('checkout', ['plan_id' => $plan->id]) }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="plan_id" value="{{ $plan->id }}">
+                        <button type="submit" class="btn btn-success mt-2">Checkout</button>
+                    </form>
+                  </div>
+              </div>
+              
+          </div>
+          @endforeach
+      </div>
+      <p id="error-message" style="color: red; display: none;">Please select a subscription option.</p>
+      <br>
+  
 
 </main>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script>
+$(document).ready(function() {
+  "use strict";
 
-    <footer class="mt-auto">
+  $('ul.navbar-nav > li').onclick(function(e) {
+    e.preventDefault();
+    $('ul.navbar-nav > li').removeClass('active');
+    $(this).addClass('active');
+  });
+});
 
-        <section class="w-100">
-          <!-- Footer -->
-          <footer class="text-center text-white" style="background-color: #5ccfa9;">
-            <!-- Grid container -->
-            <div class="container p-4 pb-0">
-              <!-- Section: CTA -->
-        
-              <section class="d-flex justify-content-center gap-4">
-                <p class=""><a href="/faq"><span class="text-dark">FAQ</span></a></p>
-                <p class=""><a href="/contactus"><span class="text-dark">Contact us!</span> </a></p> 
-              </section>
-              <!-- Section: CTA -->
-            </div>
-            <!-- Grid container -->
-        
-            
-            <div class="text-center d-flex justify-content-between" style="background-color: rgba(0, 0, 0, 0.2);" >
-          
-            <section class="p-2"><a href="/index"><img src="{{asset('images/logo-1.png')}}" class="rounded" style="width:40px;" alt="logo"></a></section>
-        
-            <section class="p-1 text-center p-100">
-              <!-- Facebook -->
-              
-              <a
-                class="btn btn-link btn-floating btn-lg text-dark m-1"
-                href="#!"
-                role="button"
-                data-mdb-ripple-color="dark"
-                ><i class="fab fa-facebook-f"></i
-              ></a>
-        
-              <!-- Twitter -->
-              <a
-                class="btn btn-link btn-floating btn-lg text-dark m-1"
-                href="#!"
-                role="button"
-                data-mdb-ripple-color="dark"
-                ><i class="fab fa-twitter"></i
-              ></a>
-        
-              <!-- Instagram -->
-              <a
-                class="btn btn-link btn-floating btn-lg text-dark m-1"
-                href="https://www.instagram.com/scootify.lux/"
-                role="button"
-                data-mdb-ripple-color="dark"
-                ><i class="fab fa-instagram"></i
-              ></a>
-              </section> 
-        
-              <!-- Back to top button -->
-              <section class="p-2">
-                <button type="button" class="btn btn-danger btn-floating btn-lg d-flex shadow-none" id="btn-back-to-top" style="background-color:black; opacity:0.7;">
-                    <i class="fas fa-arrow-up"></i>
-                </button>
-        </section>
-              
-        </div>
-        </section>
-        </footer>
-        
-        
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-        <script>
-          $(document).ready(function() {
-            "use strict";
-          
-            $('ul.navbar-nav > li').onclick(function(e) {
-              e.preventDefault();
-              $('ul.navbar-nav > li').removeClass('active');
-              $(this).addClass('active');
-            });
-          });
-        
-        
-          setTimeout(function() {
-            $('#flashMessage').fadeOut('slow');
-          }, 3000);
-        
-        
-        //Get the button
-        let mybutton = document.getElementById("btn-back-to-top");
-        
-        // When the user scrolls down 20px from the top of the document, show the button
-        window.onscroll = function () {
-        scrollFunction();
-        };
-        
-        function scrollFunction() {
-        if (
-        document.body.scrollTop > 20 ||
-        document.documentElement.scrollTop > 20
-        ) {
-        mybutton.style.display = "block";
-        } else {
-        mybutton.style.display = "none";
-        }
-        }
-        // When the user clicks on the button, scroll to the top of the document
-        mybutton.addEventListener("click", backToTop);
-        
-        function backToTop() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-        }
-        
-    // script for the subscriptions
-     function selectCard(planId) {
-            document.getElementsByName('plan_id')[0].value = planId;
-        }
 
-    function checkout() {
-        if (document.getElementById('plan_id').value === '') {
-            alert('Please select a product before proceeding to checkout.');
-        } else {
-            document.getElementById('checkout-button').type = 'submit';
-        }
-    }
+setTimeout(function() {
+  $('#flashMessage').fadeOut('slow');
+}, 3000);
+
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+scrollFunction();
+};
+
+function scrollFunction() {
+if (
+document.body.scrollTop > 20 ||
+document.documentElement.scrollTop > 20
+) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+
+// script for the subscriptions
+function selectCard(planId) {
+  document.getElementsByName('plan_id')[0].value = planId;
+}
+
+function checkout() {
+if (document.getElementById('plan_id').value === '') {
+  alert('Please select a product before proceeding to checkout.');
+} else {
+  document.getElementById('checkout-button').type = 'submit';
+}
+}
 </script>
 
-</body>
-</html>
 
+</x-layout>
